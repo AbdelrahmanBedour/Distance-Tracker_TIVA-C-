281dfcbced07be2f2ded2490720664186f0b09a1
abdwd <73134994+abdwd@users.noreply.github.com>
2021-06-04 21:05:37 +0200

    Updated Lcd.h

--- lcd.h
+++ lcd.h
@@ -21,17 +21,35 @@
 /*******************************************************************************
  *                      Preprocessor Macros                                    *
  *******************************************************************************/
+/* LCD Data bits mode configuration */
+#define DATA_BITS_MODE 8
+
+/* Use higher 4 bits in the data port */
+#if (DATA_BITS_MODE == 4)
+#define UPPER_PORT_PINS
+#endif
 
 /* LCD HW Pins */
-#define RS
-#define RW
-#define E
+#define RS                      4 /* PC4 */
+#define RW                      5 /* PC5 */
+#define E                       6 /* PC6 */
 
-#define LCD_CTRL_PORT
-#define LCD_CTRL_PORT_DIR
+#define LCD_CTRL_PORT_BASE      GPIO_PORTC_BASE_ADDRESS
+#define LCD_CTRL_PORT           GPIO_PORTC_DATA_REG
+#define LCD_CTRL_PORT_DIR       GPIO_PORTC_DIR_REG
+#define LCD_CTRL_PORT_NUM       2
 
-#define LCD_DATA_PORT
-#define LCD_DATA_PORT_DIR
+#define LCD_DATA_PORT_BASE      GPIO_PORTB_BASE_ADDRESS
+#define LCD_DATA_PORT           GPIO_PORTB_DATA_REG
+#define LCD_DATA_PORT_DIR       GPIO_PORTB_DIR_REG
+#define LCD_DATA_PORT_NUM       1
+
+/* LCD Commands */
+#define CLEAR_COMMAND 0x01
+#define FOUR_BITS_DATA_MODE 0x02
+#define TWO_LINE_LCD_Four_BIT_MODE 0x28
+#define TWO_LINE_LCD_Eight_BIT_MODE 0x38
+#define CURSOR_OFF 0x0C
 
 
 /*******************************************************************************
@@ -39,5 +57,6 @@
  *******************************************************************************/
 void LCD_sendCommand(uint8 command);
 void LCD_displayCharacter(uint8 data);
+void LCD_init(void);
 
 #endif /* LCD_H_ */
