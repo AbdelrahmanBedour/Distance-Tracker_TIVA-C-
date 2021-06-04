60bec9eef49425e8e66aa0f123c266d8bdc27ae3
Abdelrahman Mohamed Salah <67156865+AbdelrahmanBedour@users.noreply.github.com>
2021-06-04 21:15:43 +0200

    Added helper functions prototypes

--- lcd.h
+++ lcd.h
@@ -50,6 +50,8 @@
 #define TWO_LINE_LCD_Four_BIT_MODE 0x28
 #define TWO_LINE_LCD_Eight_BIT_MODE 0x38
 #define CURSOR_OFF 0x0C
+#define CURSOR_ON 0x0E
+#define SET_CURSOR_LOCATION 0x80 
 
 
 /*******************************************************************************
@@ -58,5 +60,11 @@
 void LCD_sendCommand(uint8 command);
 void LCD_displayCharacter(uint8 data);
 void LCD_init(void);
+void LCD_displayString(const char *Str);
+void LCD_clearScreen(void);
+void LCD_displayStringRowColumn(uint8 row,uint8 col,const char *Str);
+void LCD_goToRowColumn(uint8 row,uint8 col);
+void LCD_intgerToString(int data);
+
 
 #endif /* LCD_H_ */
