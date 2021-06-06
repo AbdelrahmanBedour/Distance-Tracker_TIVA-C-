652a72d1a0f53cb7d6c85732396f39d4de4aad4d
Abdelrahman Kassem <38573725+AbdelrahmanKassem@users.noreply.github.com>
2021-06-06 06:51:07 +0200

    Plotting on Google Maps

--- Plotting_Google Api.py
+++ Plotting_Google Api.py
@@ -10,9 +10,6 @@ with open('coordinates.txt','r') as f:
         latitude_list.append(float(row[0]))
         longitude_list.append(float(row[1]))
   
-#latitude_list = [ 30.3358376, 30.307977, 30.3216419 ]
-#longitude_list = [ 77.8701919, 78.048457, 78.0413095 ]
-  
 gmap3 = gmplot.GoogleMapPlotter(latitude_list[int(len(latitude_list)/2)],longitude_list[int(len(longitude_list)/2)], 13)
   
 # scatter method of map object 
@@ -25,5 +22,4 @@ gmap3.scatter( latitude_list, longitude_list, '# FF0000', size = 90, marker = Fa
 gmap3.plot(latitude_list, longitude_list,'cornflowerblue', edge_width = 4)
 
 gmap3.apikey = "AIzaSyDeRNMnZ__VnQDiATiuz4kPjF_c9r1kWe8"
-#gmap3.apikey = "AIzaSyCVK3o95IbZybALGz3IbB7WTvqxzIvsL48" #mine
 gmap3.draw('Gmap.html')
