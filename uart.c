c86e981385ce610b0a833ffe2b6b896d08ce70ef
Shams Elgohary <68311964+ShamsElgohary@users.noreply.github.com>
2021-06-13 13:06:48 +0200

    Co-op with the new .h file

--- uart.c
+++ uart.c
@@ -6,15 +6,15 @@
 
 void UART0_init(void)
 {
-SYSCTL_RCGCUART_REG |= 1;                                   // ACTIVATE UART0 CLOCK
-SYSCTL_REGCGC2_REG |= 1;                                   // ACTIVATE CLOCK FOR PORT A
+SYSCTL_RCGCUART_REG |= UART0_CLOCK;                                   // ACTIVATE UART0 CLOCK
+SYSCTL_REGCGC2_REG  |= PORTA_CLOCK;                                   // ACTIVATE CLOCK FOR PORT A
 
 UART0_CTL_R  =0;                                           // DISBALE UART
 /* DECLARING THE BAUD RATE  */
 UART0_IBRD_R = (USART_DIVINT_VALUE);                       //UART-_IBRD_R = 104  , MASKING ISN'T NESSECERALLY NEEDED HERE VALUE IS VERY LOW IN COMPARSION
 UART0_FBRD_R = (USART_DIVFRAC_VALUE);                      //UART-_FBRD_R = 11
-UART0_LCRH_R = 0x0070;                                                                          // WLEN = 11 (DATA BITS = 8) FEN =1 (ENABLE 16 ELEMENTS IN BUFFER)
-UART0_CTL_R  = 0x0301;                                                                          // ENABLE UART , ENABLE TRANSMITTER ,ENABLE RECEIVER
+UART0_LCRH_R = LCRH_ENABLE;                                // WLEN = 11 (DATA BITS = 8) FEN =1 (ENABLE 16 ELEMENTS IN BUFFER)
+UART0_CTL_R  = CTL_ENABLE;                                 // ENABLE UART , ENABLE TRANSMITTER ,ENABLE RECEIVER
                      
 GPIO_PORTA_AMSEL_REG &= ~(PORTA_PIN0_PIN1);                  // WE WANT THIS PIN DIGITAL -> GPIO_PORTA_AMSEL_REG &= ~0X03
 GPIO_PORTA_AFSEL_REG |= (PORTA_PIN0_PIN1);                   // TO ACTIVATE ALTERNATIVE FUNCTION SELECT FOR PIN0 AND PIN1
@@ -43,15 +43,15 @@ uint8 UART0_recieveByte(void)
 
 void UART1_init(void)
 {
-SYSCTL_RCGCUART_REG |= 0x0020;                                               // ACTIVATE UART5 CLOCK        0b0010 0000
-SYSCTL_REGCGC2_REG |=  0x0010;                            //0b00000100      // ACTIVATE CLOCK FOR PORT E
+SYSCTL_RCGCUART_REG |= UART5_CLOCK;                                               // ACTIVATE UART5 CLOCK        0b0010 0000
+SYSCTL_REGCGC2_REG  |= PORTE_CLOCK;                            //0b00000100      // ACTIVATE CLOCK FOR PORT E
 
 UART5_CTL_R  =0;                                           // DISBALE UART
 /* DECLARING THE BAUD RATE  */
 UART5_IBRD_R = (USART_DIVINT_VALUE);                       //UART-_IBRD_R = 104  , MASKING ISN'T NESSECERALLY NEEDED HERE VALUE IS VERY LOW IN COMPARSION
 UART5_FBRD_R = (USART_DIVFRAC_VALUE);                      //UART-_FBRD_R = 11
-UART5_LCRH_R = 0x0070;                                                                          // WLEN = 11 (DATA BITS = 8) FEN =1 (ENABLE 16 ELEMENTS IN BUFFER)
-UART5_CTL_R  = 0x0301;                                                                          // ENABLE UART , ENABLE TRANSMITTER ,ENABLE RECEIVER
+UART5_LCRH_R = LCRH_ENABLE;                                // WLEN = 11 (DATA BITS = 8) FEN =1 (ENABLE 16 ELEMENTS IN BUFFER)
+UART5_CTL_R  = CTL_ENABLE;                                 // ENABLE UART , ENABLE TRANSMITTER ,ENABLE RECEIVER
                      
 GPIO_PORTE_AMSEL_REG &= ~(PORTE_PIN4_PIN5);                  // WE WANT THIS PIN DIGITAL -> GPIO_PORTC_AMSEL_REG
 GPIO_PORTE_AFSEL_REG |= (PORTE_PIN4_PIN5);                   // TO ACTIVATE ALTERNATIVE FUNCTION SELECT FOR PIN4 AND PIN5
